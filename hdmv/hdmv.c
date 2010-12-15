76f6ced4458b26c1be7f5a7061f1900794514044
Kieran Kunhya <kieran@kunhya.com>
2010-12-15 18:56:31 +0000

    Add HDMV stuff

---
+++ hdmv/hdmv.c
@@ -0,0 +1,136 @@
+/*****************************************************************************
+ * hdmv.c : HDMV specific functions
+ *****************************************************************************
+ * Copyright (C) 2010 Kieran Kunhya
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *****************************************************************************/
+
+#include "../common.h"
+#include "hdmv.h"
+
+int ts_setup_hdmv_video_stream( ts_writer_t *w, int pid, int frame_rate, int aspect_ratio )
+{
+    ts_int_stream_t *stream = find_stream( w, pid );
+
+    if( !stream )
+    {
+        fprintf( stderr, "Invalid PID\n" );
+        return -1;
+    }
+
+    stream->hdmv_video_ctx = calloc( 1, sizeof(stream->hdmv_video_ctx) );
+    if( !stream->hdmv_video_ctx )
+        return -1;
+
+    stream->hdmv_video_ctx->frame_rate = frame_rate;
+    stream->hdmv_video_ctx->aspect_ratio = aspect_ratio;
+
+    return 0;
+}
+
+int ts_setup_hdmv_lpcm_stream( ts_writer_t *w, int pid, int num_channels, int sample_rate, int bits_per_sample )
+{
+    ts_int_stream_t *stream = find_stream( w, pid );
+
+    if( !stream )
+    {
+        fprintf( stderr, "Invalid PID\n" );
+        return -1;
+    }
+
+    stream->lpcm_ctx = calloc( 1, sizeof(stream->lpcm_ctx) );
+    if( !stream->lpcm_ctx )
+        return -1;
+
+    stream->lpcm_ctx->num_channels = num_channels;
+    stream->lpcm_ctx->sample_rate = sample_rate;
+    stream->lpcm_ctx->bits_per_sample = bits_per_sample;
+
+    return 0;
+}
+
+int ts_setup_dtcp( ts_writer_t *w, uint8_t byte_1, uint8_t byte_2 )
+{
+    w->dtcp_ctx = calloc( 1, sizeof(w->dtcp_ctx) );
+    if( !w->dtcp_ctx )
+        return -1;
+
+    w->dtcp_ctx->byte_1 = byte_1;
+    w->dtcp_ctx->byte_2 = byte_2;
+
+    return 0;
+}
+
+/* First loop of PMT Descriptors */
+void write_hdmv_copy_control_descriptor( ts_writer_t *w )
+{
+    bs_t *s = &w->out.bs;
+
+    bs_write( s, 8, HDMV_COPY_CTRL_DESCRIPTOR_TAG ); // descriptor_tag
+    bs_write( s, 8, 0x04 );           // descriptor_length
+    bs_write( s, 16, 0x0fff );        // CA_System_ID
+    bs_write( s, 8, w->dtcp_ctx->byte_1 ); // private_data_byte
+    bs_write( s, 8, w->dtcp_ctx->byte_2 ); // private_data_byte
+}
+
+/* Second loop of PMT Descriptor */
+void write_hdmv_lpcm_descriptor( ts_writer_t *w, ts_int_stream_t *stream )
+{
+    bs_t *s = &w->out.bs;
+
+    write_registration_descriptor( s, HDMV_LPCM_REG_DESCRIPTOR_TAG, 8, "HDMV" );
+    bs_write( s, 8, 0xff );                // stuffing_bits
+    bs_write( s, 8, stream->stream_type ); // stream_coding_type
+
+    if( stream->lpcm_ctx->num_channels == 1 )
+        bs_write( s, 4, 1 );    // audio_presentation_type
+    else if( stream->lpcm_ctx->num_channels == 2 )
+        bs_write( s, 4, 0x03 ); // audio_presentation_type
+    else
+        bs_write( s, 4, 0x06 ); // audio_presentation_type
+
+    if( stream->lpcm_ctx->sample_rate == 48 )
+        bs_write( s, 4, 1 );    // sampling_frequency
+    else if( stream->lpcm_ctx->sample_rate == 96 )
+        bs_write( s, 4, 0x04 ); // sampling_frequency
+    else
+        bs_write( s, 4, 0x05 ); // sampling_frequency
+
+    if( stream->lpcm_ctx->bits_per_sample == 16 )
+        bs_write( s, 2, 1 );    // bits_per_sample
+    else if( stream->lpcm_ctx->bits_per_sample == 20 )
+        bs_write( s, 2, 0x02 ); // bits_per_sample
+    else
+        bs_write( s, 2, 0x03 ); // bits_per_sample
+
+    bs_write( s, 6, 0x3f );     // stuffing_bits
+
+}
+
+/* In loop of SIT */
+void write_partial_ts_descriptor( ts_writer_t *w )
+{
+    bs_t *s = &w->out.bs;
+
+    bs_write( s, 8, HDMV_PARTIAL_TS_DESCRIPTOR_TAG ); // descriptor_tag
+    bs_write( s, 8, 0x08 );      // descriptor_length
+    bs_write( s, 2, 0x03 );      // DVB_reserved_future_use
+    bs_write( s, 22, w->ts_muxrate / 400 ); // peak_rate
+    bs_write( s, 2, 0x03 );      // DVB_reserved_future_use
+    bs_write( s, 22, 0x3fffff ); // minimum_overall_smoothing_rate
+    bs_write( s, 2, 0x03 );      // DVB_reserved_future_use
+    bs_write( s, 14, 0x3fff );   // maximum_overall_smoothing_buffer
+}
---
+++ hdmv/hdmv.h
@@ -0,0 +1,55 @@
+/*****************************************************************************
+ * hdmv.h : HDMV specific headers
+ *****************************************************************************
+ * Copyright (C) 2010 Kieran Kunhya
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *****************************************************************************/
+
+#ifndef LIBMPEGTS_HDMV_H
+#define LIBMPEGTS_HDMV_H
+
+/* Blu-Ray specific stream types */
+#define AUDIO_LPCM                     0x80
+#define AUDIO_DTS                      0x82
+#define AUDIO_DOLBY_LOSSLESS           0x83
+#define AUDIO_DTS_HD                   0x85
+#define AUDIO_DTS_HD_XLL               0x86
+#define AUDIO_EAC3_SECONDARY           0xa1
+#define AUDIO_DTS_HD_SECONDARY         0xa2
+#define SUB_PRESENTATION_GRAPHICS      0x90 
+#define SUB_INTERACTIVE_GRAPHICS       0x91
+#define SUB_TEXT                       0x92
+
+/* Blu-Ray Aspect Ratios */
+#define HDMV_4_3_AR                    0x02
+#define HDMV_16_9_AR                   0x03
+
+/* Registration Descriptor Tags */
+#define HDMV_REG_DESCRIPTOR_TAG        0x05
+#define HDMV_VID_REG_DESCRIPTOR_TAG    0x05
+#define HDMV_LPCM_REG_DESCRIPTOR_TAG   0x05
+
+/* Descriptor Tags */
+#define HDMV_PARTIAL_TS_DESCRIPTOR_TAG 0x63
+#define HDMV_AC3_DESCRIPTOR_TAG        0x81
+#define HDMV_CAPTION_DESCRIPTOR_TAG    0x86
+#define HDMV_COPY_CTRL_DESCRIPTOR_TAG  0x88
+
+void write_hdmv_copy_control_descriptor( ts_writer_t *w );
+void write_hdmv_lpcm_descriptor( ts_writer_t *w, ts_int_stream_t *stream );
+void write_partial_ts_descriptor( ts_writer_t *w );
+
+#endif
