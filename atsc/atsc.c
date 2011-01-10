f37e134a19f67913a65c2728c3339222253257c1
Kieran Kunhya <kieran@kunhya.com>
2011-01-10 14:04:37 +0000

    Add skeleton of ATSC support.

--- atsc/atsc.c
+++ atsc/atsc.c
@@ -21,6 +21,37 @@
 #include "../common.h"
 #include "atsc.h"
 
-/* First Loop of PMT */
+/* Second Loop of PMT */
+void write_atsc_ac3_descriptor( bs_t *s )
+{
+    bs_write( s, 8, ATSC_AC3_DESCRIPTOR_TAG );
+}
 
+/* EIT */
+// FIXME fill in the blanks
+void write_caption_service_descriptor( bs_t *s )
+{
+    bs_write( s, 8, ATSC_CAPTION_SERVICE_DESCRIPTOR_TAG ); // descriptor_tag
+    bs_write( s, 8, 0 ); // descriptor_length
+    bs_write( s, 3, 0x7 ); // reserved
 
+    bs_write( s, 5, 0 ); // number_of_services
+
+    for( int i = 0; i < 0; i++ )
+    {
+        bs_write( s, 8, 0 ); // language[0]
+        bs_write( s, 8, 0 ); // language[1]
+        bs_write( s, 8, 0 ); // language[2]
+        bs_write1( s, 0 );   // digital_cc
+        bs_write1( s, 1 );   // reserved
+
+        if( 0 )
+            bs_write( s, 6, 0x3f ); // reserved
+        else
+            bs_write( s, 6, 0 ); // caption_service_number
+
+        bs_write1( s, 0 ); // easy_reader
+        bs_write1( s, 0 ); // wide_aspect_ratio
+        bs_write( s, 13, 0x3fff ); // reserved
+    }
+}
